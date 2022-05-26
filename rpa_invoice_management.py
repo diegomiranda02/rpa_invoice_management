5964fb01f1fa5535575541251afd4eaa508b1fbe
Diego Miranda <diego.miranda02@gmail.com>
2022-05-26 15:24:08 -0300

    Implementando acesso ao site do Directa Natal

--- rpa_invoice_management.py
+++ rpa_invoice_management.py
@@ -1,2 +1,19 @@
 import pyautogui
 import sys
+
+# Point(x=37, y=964) - Position of the Windows Button
+pyautogui.moveTo(x=37, y=964)
+
+# Click the Windows Button
+pyautogui.click(x=37, y=964, button='left')
+
+pyautogui.PAUSE = 2
+
+# Point(x=834, y=68) - Position of the search bar
+pyautogui.moveTo(x=834, y=68)
+
+# Click the search bar
+pyautogui.click(x=834, y=68, button='left')
+
+# Type Firefox
+pyautogui.write("Firefox", interval=0.25)
